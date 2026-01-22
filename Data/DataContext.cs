61cea1800d9f554ce5ada88efc7dacb4c5b81def
Vladimir <korotnitskivladimir@gmail.com>
2026-01-22 20:31:04 +0200

    Project Created

---
+++ Data/DataContext.cs
@@ -0,0 +1,54 @@
+﻿using AMZN.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMZN.Data;
+
+public class DataContext: DbContext
+{
+    public DbSet<UserData> UsersData { get; private set; }
+    public DbSet<UserAccess> UsersAccess { get; private set; } 
+    public DbSet<UserRole> UserRoles { get; private set; }
+    
+
+    public DataContext(DbContextOptions options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("AMZN");
+
+        modelBuilder.Entity<UserAccess>()
+            .HasIndex(ua => ua.Username)
+            .IsUnique();
+
+        modelBuilder.Entity<UserAccess>()
+            .HasOne(ua => ua.UserData)
+            .WithMany()
+            .HasForeignKey(ua => ua.UserId)
+            .HasPrincipalKey(ud => ud.Id);
+
+        modelBuilder.Entity<UserAccess>()
+            .HasOne(ua => ua.UserRole)
+            .WithMany()
+            .HasForeignKey(ua => ua.RoleId);
+
+        modelBuilder.Entity<UserRole>().HasData(
+            new UserRole()
+            {
+                Id = "guest", Description = "solely registered user", CanCreate = 0, CanRead = 0, CanUpdate = 0,
+                CanDelete = 0
+            },
+            new UserRole()
+            {
+                Id = "admin", Description = "full access to DB", CanCreate = 1, CanRead = 1, CanUpdate = 1,
+                CanDelete = 1
+            },
+            new UserRole()
+            {
+                Id = "moderator", Description = "can block", CanCreate = 0, CanRead = 1, CanUpdate = 0, CanDelete = 1
+            },
+            new UserRole()
+            {
+                Id = "editor", Description = "can edit", CanCreate = 0, CanRead = 1, CanUpdate = 1, CanDelete = 0
+            });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+using AMZN.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddDbContext<DataContext>(options => options
+    .UseSqlServer(builder
+        .Configuration.GetConnectionString("LocalMs")));
+
+builder.Services.AddCors(options => 
+    options.AddDefaultPolicy(policy => { policy.AllowAnyOrigin().AllowAnyHeader();
+    }));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors();
+app.UseAuthorization();
+app.UseSession();
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
