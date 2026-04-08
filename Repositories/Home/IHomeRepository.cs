ca6c4592d8d0fe5dece85b5f3ca143adcb168164
Denis <bigdramashow730@gmail.com>
2026-04-08 17:14:00 +0300

    refactor home data access

--- Repositories/Home/IHomeRepository.cs
+++ Repositories/Home/IHomeRepository.cs
@@ -1,11 +1,19 @@
 ﻿using AMZN.Data.Entities;
+using AMZN.DTOs.Home;
 
 namespace AMZN.Repositories.Home
 {
     public interface IHomeRepository
     {
         Task<List<Category>> GetAllCategoriesAsync();
-        Task<List<Product>> GetStockProductsAsync();
-        Task<List<Product>> GetLastViewedProductsByIdsAsync(List<Guid> productIds);
+        Task<List<CategoryProductCountsDto>> GetCategoryProductCountsAsync(decimal minRating);
+
+        Task<List<Product>> GetTopRatedProductsForCategoriesAsync(List<Guid> categoryIds, decimal minRating, int take);
+
+        Task<List<Product>> GetTopRatedProductsAsync(decimal minRating, int take);
+
+        Task<List<Product>> GetTopRatedProductsUnderPriceAsync(decimal maxPrice, decimal minRating, int take);
+
+        Task<List<Product>> GetProductsByIdsAsync(List<Guid> productIds);
     }
 }
