eda0312432a0f346856c829f1c769c2a17b79780
Denis <bigdramashow730@gmail.com>
2026-04-15 14:52:56 +0300

    product gallery editor

---
+++ Models/Product/ProductGalleryEditorViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace AMZN.Models.Product
+{
+    public class ProductGalleryEditorViewModel
+    {
+        // Заголовок драгндроп блока галереи в UI
+        public string Title { get; set; } = "Gallery images";
+
+        // Имя file-input поля, которое должно попасть в model binding
+        // Create -> Form.Images
+        // Edit   -> Form.NewGalleryImages
+        public string NewFilesInputName { get; set; } = null!;
+
+        // Имя поля, для которого надо показать validation message под компонентом
+        // Обычно совпадает с NewFilesInputName
+        // Create -> Form.Images
+        // Edit   -> Form.NewGalleryImages
+        public string? ValidationMessageFieldName { get; set; }
+
+
+        // Префикс hidden-полей с порядком existing изображений
+        // Нужен только в Edit
+        public string? ExistingIdsFieldPrefix { get; set; }
+
+        // Уже сохранённые изображения
+        // Для Create пустой список
+        public List<ExistingProductImageViewModel> ExistingImages { get; set; } = new();
+
+        public int MaxGalleryImages { get; set; } = 10;
+
+        // Максимальный размер одного файла в байтах.
+        public long MaxFileBytes { get; set; } = 5 * 1024 * 1024;
+
+        // Разрешать drag-and-drop сортировку уже существующих изображений
+        // Edit -> true || Create -> false
+        public bool AllowExistingReorder { get; set; }
+
+        // Режим компонента
+        // true  -> Edit || false -> Create
+        public bool IsEditMode { get; set; }
+    }
+}
